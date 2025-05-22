c4779e6759117faa2f2aea47eb430db228ffad17
erzh <ergatapovaer@gmail.com>
2025-05-22 16:42:40 +0400

    added restart

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import globals from "globals";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.node, ...globals.es2021} }},
+  js.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.es2021 }
+    }
+  }
 ];
