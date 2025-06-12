7e7b025327946237d10984c4bbaf18ac7d542f84
erzh <ergatapovaer@gmail.com>
2025-06-12 17:01:18 +0400

    changed exec mode for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      exec_mode: 'fork',
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production'
